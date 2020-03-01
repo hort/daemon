b8ebc2ddc0af923483325aaf98b693742aab17b7
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-03-02 00:43:16 +0100

    fix(worker): fix bad design issues

--- include/hort/worker.hpp
+++ include/hort/worker.hpp
@@ -1,24 +1,22 @@
 #ifndef WORKER_HPP_
 #define WORKER_HPP_
 
+#include "hort/chrono.hpp"
+
 #include <functional>
-#include <queue>
 #include <mutex>
+#include <queue>
 #include <thread>
-
-#include "hort/interface.hpp"
-
-#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
-#define PBWIDTH 60
 
 namespace hort
 {
 
 namespace detail {
 
-/// \brief Thread safe task queue
-class Tasks : public std::queue<std::function<void(void)>> {
-	using callback_t = std::function<void(void)>;
+/// \brief Thread safe queue
+template <typename T>
+class Tasks : public std::queue<T> {
+	using callback_t = T;
 
 	std::mutex mutex;
 
@@ -39,11 +37,10 @@ public:
 
 }
 
-class Worker : public detail::Tasks {
-
+class Worker {
 	using callback_t = std::function<void(void)>;
 
-	std::queue<callback_t> queue;
+	detail::Tasks<callback_t> queue;
 	std::vector<std::thread> threads;
 
 	bool close_request;
@@ -54,17 +51,16 @@ class Worker : public detail::Tasks {
 public:
 	Worker()
 		: queue{}
-		, threads{5}
+		, threads{1}
 		, close_request{false}
 		, tasks_total{0}
 		, tasks_completed{0}
 	{
 		auto t = [this]() {
-			while (!close_request || size()) {
-				if (size()) {
-					callback_t callback = pop();
+			while (!close_request || queue.size()) {
+				if (queue.size()) {
+					callback_t callback = queue.pop();
 					callback();
-
 					tasks_completed += 1;
 				}
 			}
@@ -87,21 +83,23 @@ public:
 	}
 
 	void push(const callback_t &callback) {
-		detail::Tasks::push(callback);
-
+		queue.push(callback);
 		tasks_total += 1;
 	}
 
 	void progress()
 	{
-		while(size()) {
-			float percentage = clamp((float)tasks_completed / tasks_total, 0, 100);
+		const char *PBSTR   = "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+		const int   PBWIDTH = 60;
+
+		while (queue.size()) {
+			float percentage = std::clamp((float)tasks_completed / tasks_total, (float)0, (float)100);
 			int val  = (int)(percentage * 100);
 			int lpad = (int)(percentage * PBWIDTH);
 			int rpad = PBWIDTH - lpad;
-			printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+			printf("\r%3d%% [%.*s%*s] %d/%d", val, lpad, PBSTR, rpad, "", tasks_completed, tasks_total);
 			fflush(stdout);
-			sleep(100);
+			hort::chrono::sleep(100);
 		}
 	}
 
