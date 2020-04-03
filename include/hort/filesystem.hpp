1ca48971ed225ed2ddc468d1b93e1434a682c96a
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-04-03 22:11:15 +0200

    refactor(filesystem): overhaul module

--- include/hort/filesystem.hpp
+++ include/hort/filesystem.hpp
@@ -1,20 +1,33 @@
-#ifndef FILESYSTEM_HPP_
-#define FILESYSTEM_HPP_
+#ifndef HORT_FILESYSTEM_HPP_
+#define HORT_FILESYSTEM_HPP_
 
-#include <string>
+#include "hort/string.hpp"
 
-namespace hort::filesystem
-{
+namespace hort::filesystem {
 
 /// \brief Returns true if file exists.
-bool exists(const std::string &name);
+[[nodiscard]] inline bool exists(const std::string& path);
 
-/// \brief Create directory at path. If parent directories don't exist, create them.
-int mkpath(std::string path);
+/// \brief Returns true if file exists.
+template <typename... Args>
+[[nodiscard]] inline bool exists(const Args&... args) {
+  return exists("/"_join(args...));
+}
 
-/// \brief Save bin to filepath/filename. Does not check if file already exists.
-int save(const std::string &bin, const std::string &filepath, const std::string &filename);
+/// \brief Create directories to path.
+inline int mkpath(std::string path);
+
+/// \brief Save bin to filepath/filename. If file already exists overwrites it.
+inline int write(const std::string& source,
+                 const std::string& filepath,
+                 const std::string& filename);
+
+/// \brief Sanitize filepath replacing all occurences of '/' with '-'.
+[[nodiscard]] inline std::string sanitize(const std::string& str);
+
+/// \brief Returns the base nome of the path.
+[[nodiscard]] inline std::string base_name(const std::string& path);
 
 } // namespace hort::filesystem
 
-#endif /* FILESYSTEM_HPP_ */
+#endif /* HORT_FILESYSTEM_HPP_ */
--- src/hort/filesystem.cpp
+++ src/hort/filesystem.cpp
@@ -1,50 +1,63 @@
 #include "hort/filesystem.hpp"
+#include "hort/regexp.hpp"
 
 #include <fstream>
 #include <sys/stat.h>
 #include <unistd.h>
 
-namespace hort::filesystem
-{
+namespace hort::filesystem {
 
-bool exists(const std::string &name)
-{
-	struct stat buffer;
-	return (stat (name.data(), &buffer) == 0);
+bool exists(const std::string& path) {
+  struct stat buffer;
+  return (stat(name.data(), &buffer) == 0);
 }
 
-int mkpath(std::string path)
-{
-	if (exists(path)) return 0;
+int mkpath(std::string path) {
+  if (exists(path)) return 0;
 
-	int pos = 0;
-	std::string dir;
-	int mdret;
+  std::string::size_type pos = 0;
+  std::string dir;
+  int mdret;
 
-	if(path[path.size() - 1] != '/')
-		path += '/';
+  if (path[path.size() - 1] != '/') {
+    path += '/';
+  }
 
-	while((pos = path.find_first_of('/', pos)) != std::string::npos) {
-		dir = path.substr(0, pos++);
-		if(dir.size() == 0)
-			continue;
-		if((mdret = mkdir(dir.c_str(), 0775)) && errno != EEXIST)
-			return mdret;
-	}
+  while ((pos = path.find_first_of('/', pos)) != std::string::npos) {
+    dir = path.substr(0, pos++);
+    if (dir.size() == 0) continue;
+    if ((mdret = mkdir(dir.c_str(), 0775)) && errno != EEXIST) {
+      return mdret;
+    }
+  }
 
-	return mdret;
+  return mdret;
 }
 
-int save(const std::string &bin, const std::string &filepath, const std::string &filename)
-{
-	mkpath(filepath);
+int write(const std::string& source,
+          const std::string& filepath,
+          const std::string& filename) {
+  mkpath(filepath);
+  std::ofstream file;
+  file.open(filepath + "/" + filename);
+  if (file.fail())
+    return 1;
+  file << source;
+  file.close();
+  return 0;
+}
 
-	std::ofstream file;
-	file.open(filepath + "/" + filename);
-	if (file.fail()) return 1;
-	file << bin;
-	file.close();
-	return 0;
+void sanitize(std::string& str) {
+  hort::regexp::replaceall("/", "-", str);
+}
+
+std::string base_name(const std::string &path) {
+  auto pos = path.find_last_of("/");
+  if (pos != std::string::npos) {
+    return path.substr(pos, path.length());
+  } else {
+    return path;
+  }
 }
 
 } // namespace hort::filesystem
