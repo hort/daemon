2a5dfac397bfc53c359c4f6a065146e46d155284
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-04-05 23:29:25 +0200

    style(format): update to new style guides

--- include/hort/formats.hpp
+++ include/hort/formats.hpp
@@ -1,35 +1,33 @@
-#ifndef XML2JSON_HPP_
-#define XML2JSON_HPP_
+#ifndef HORT_FORMATS_HPP_
+#define HORT_FORMATS_HPP_
 
-#include <yaml-cpp/yaml.h>
 #include <rapidxml.hpp>
+#include <yaml-cpp/yaml.h>
 
 #include "json.hpp"
 using json = nlohmann::json;
 
-namespace hort::formats
-{
+namespace hort::formats {
 
-namespace detail
-{
+namespace detail {
 
-json xml2json(rapidxml::xml_node<> *root);
+json xml2json(rapidxml::xml_node<>* root);
 json yaml2json(YAML::Node root);
 
 } // namespace detail
 
 /// \brief Convert an xml encoded string into a json object
-[[nodiscard]] json xml2json(const std::string &str);
+[[nodiscard]] json xml2json(const std::string& str);
 
 /// \brief Convert an yaml encoded string into a json object
-[[nodiscard]] json yaml2json(const std::string &str);
+[[nodiscard]] json yaml2json(const std::string& str);
 
 /// \brief Load a file containing a xml document into a json
-[[nodiscard]] json loadxml(const std::string &filepath);
+[[nodiscard]] json loadxml(const std::string& filepath);
 
 /// \brief Load a file containing a yaml document into a nlohmann::json object.
-[[nodiscard]] json loadyaml(const std::string &filepath);
+[[nodiscard]] json loadyaml(const std::string& filepath);
 
 } // namespace hort::formats
 
-#endif /* XML2JSON_HPP_ */
+#endif /* HORT_FORMATS_HPP_ */
--- src/hort/formats.cpp
+++ src/hort/formats.cpp
@@ -2,131 +2,120 @@
 
 #include <fstream>
 
-namespace hort::formats
-{
+namespace hort::formats {
 
-namespace detail
-{
+namespace detail {
 
-json xml2json(rapidxml::xml_node<> *root)
-{
-	json j{};
-	std::map<std::string, int> occurrence{};
+json xml2json(rapidxml::xml_node<>* root) {
+  json j{};
+  std::map<std::string, int> occurrence{};
 
-	for (auto *node = root->first_node(); node; node = node->next_sibling()) {
-		const char *key = node->name();
-		occurrence[key] += 1;
+  for (auto* node = root->first_node(); node; node = node->next_sibling()) {
+    const char* key = node->name();
+    occurrence[key] += 1;
 
-		// if already seen, convert it into a list
-		if (occurrence[key] == 2)
-			j[key] = { j[key] };
+    if (occurrence[key] == 2) {
+      j[key] = {j[key]};
+    }
 
-		// if the node is a sequence
-		if (occurrence[key] > 1) {
-			// create a new json object and push it into the vector
-			json n{};
-			if(node->first_node())
-				n[key] = xml2json(node);
-			else
-				n["#text"] = node->value();
-			// iterate through the attributes
-			for (auto *attr = node->first_attribute(); attr; attr = attr->next_attribute())
-				n[key][attr->name()] = attr->value();
-			j[key].emplace_back(n[key]);
-		} else {
-			if (node->first_node()) {
-				j[key] = xml2json(node);
-			} else {
-				j["#text"] = node->value();
-			}
-			// iterate through the attributes
-			for (auto *attr = node->first_attribute(); attr; attr = attr->next_attribute())
-				j[key][attr->name()] = attr->value();
-		}
-	}
+    if (occurrence[key] > 1) {
+      json n{};
+      if (node->first_node()) {
+        n[key] = xml2json(node);
+      }
+      else {
+        n["#text"] = node->value();
+      }
 
-	return j;
+      for (auto* attr = node->first_attribute(); attr; attr = attr->next_attribute()) {
+        n[key][attr->name()] = attr->value();
+      }
+
+      j[key].emplace_back(n[key]);
+
+    } else {
+
+      if (node->first_node()) {
+        j[key] = xml2json(node);
+      } else {
+        j["#text"] = node->value();
+      }
+
+      for (auto* attr = node->first_attribute(); attr; attr = attr->next_attribute()) {
+        j[key][attr->name()] = attr->value();
+      }
+    }
+  }
+
+  return j;
 }
 
-json yaml2json(YAML::Node root)
-{
-	json j{};
-	switch (root.Type()) {
-	case YAML::NodeType::Null:
-		break;
-	case YAML::NodeType::Scalar:
-		return root.as<std::string>();
-	case YAML::NodeType::Sequence:
-		for (auto node : root)
-			j.emplace_back(yaml2json(node));
-		break;
-	case YAML::NodeType::Map:
-		for (const auto &it : root) {
-			YAML::Node key   = it.first;
-			YAML::Node value = it.second;
-			j[key.as<std::string>()] = yaml2json(value);
-		}
-		break;
-	default:
-		break;
-	}
-	return j;
+json yaml2json(YAML::Node root) {
+  json j{};
+  switch (root.Type()) {
+  case YAML::NodeType::Null:
+    break;
+  case YAML::NodeType::Scalar:
+    return root.as<std::string>();
+  case YAML::NodeType::Sequence:
+    for (auto node : root)
+      j.emplace_back(yaml2json(node));
+    break;
+  case YAML::NodeType::Map:
+    for (const auto& it : root) {
+      YAML::Node key           = it.first;
+      YAML::Node value         = it.second;
+      j[key.as<std::string>()] = yaml2json(value);
+    }
+    break;
+  default:
+    break;
+  }
+  return j;
 }
 
 } // namespace detail
 
-json xml2json(const std::string &str)
-{
-	json j{};
-	rapidxml::xml_document<> doc;
-	try {
-		// might be somewhat dangerous
-		doc.parse<0>(const_cast<char*>(str.data()));
-	} catch (const rapidxml::parse_error &e) {
-		return nullptr;
-	} catch (const std::exception &e) {
-		return nullptr;
-	}
-	rapidxml::xml_node<> *root = doc.first_node();
-	if (root)
-		j[root->name()] = detail::xml2json(root);
-	return j;
+json xml2json(const std::string& str) {
+  json j{};
+  rapidxml::xml_document<> doc;
+  try {
+    // might be somewhat dangerous
+    doc.parse<0>(const_cast<char*>(str.data()));
+  } catch (const rapidxml::parse_error& e) {
+    return nullptr;
+  } catch (const std::exception& e) { return nullptr; }
+  rapidxml::xml_node<>* root = doc.first_node();
+  if (root)
+    j[root->name()] = detail::xml2json(root);
+  return j;
 }
 
-
-json yaml2json(const std::string &str)
-{
-	YAML::Node root;
-	try {
-			root = YAML::Load(str);
-	} catch (const YAML::ParserException &e) {
-		return nullptr;
-	} catch (const std::exception &e) {
-		return nullptr;
-	}
-	return detail::yaml2json(root);
+json yaml2json(const std::string& str) {
+  YAML::Node root;
+  try {
+    root = YAML::Load(str);
+  } catch (const YAML::ParserException& e) {
+    return nullptr;
+  } catch (const std::exception& e) { return nullptr; }
+  return detail::yaml2json(root);
 }
 
-json loadyaml(const std::string &filepath)
-{
-	YAML::Node root;
-	try {
-		root = YAML::LoadFile(filepath);
-	} catch (const YAML::ParserException &e) {
-		return nullptr;
-	} catch (const std::exception& e) {
-		return nullptr;
-	}
-	return detail::yaml2json(root);
+json loadyaml(const std::string& filepath) {
+  YAML::Node root;
+  try {
+    root = YAML::LoadFile(filepath);
+  } catch (const YAML::ParserException& e) {
+    return nullptr;
+  } catch (const std::exception& e) { return nullptr; }
+  return detail::yaml2json(root);
 }
 
-json loadxml(const std::string &filepath)
-{
-	std::ifstream file(filepath);
-	std::string str((
-		std::istream_iterator<char>(file)),
-		std::istream_iterator<char>());
-	return xml2json(str);
+json loadxml(const std::string& filepath) {
+  std::ifstream file(filepath);
+  std::string str((std::istream_iterator<char>(file)),
+                  std::istream_iterator<char>());
+  return xml2json(str);
 }
 
 } // namespace hort::formats
