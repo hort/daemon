f97f348303404ce7f20700ca0398d7b35daa79de
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-04-08 00:10:19 +0200

    feat(args): add reference arguments

--- include/hort/args.hpp
+++ include/hort/args.hpp
@@ -1,7 +1,7 @@
 #ifndef HORT_ARGS_HPP_
 #define HORT_ARGS_HPP_
 
-#include <fmt/format.h>
+#include <functional>
 #include <map>
 #include <string>
 #include <variant>
@@ -11,6 +11,7 @@
 namespace hort {
 
 // TODO support numeric and string list arguments
+// TODO add support for variable binding
 class Args {
 
   struct Argument {
@@ -19,10 +20,14 @@ class Args {
     const std::string description;
 
     const std::variant<
-        std::function<void()>,
-        std::function<void(int)>,
-        std::function<void(std::string)>
-      > callback;
+      std::reference_wrapper<bool>,
+      std::reference_wrapper<int>,
+      std::reference_wrapper<std::string>,
+      std::function<void()>,
+      std::function<void(int)>,
+      std::function<void(std::string)>
+    > callback;
+
   };
 
   std::vector<Argument> options{};
@@ -33,16 +38,27 @@ class Args {
 
   /// \brief Width of the longest option flag.
   /// \see hort::Args::help for use.
-  int  max_width    = 0;
+  std::size_t max_width = 0;
+
   bool exit_on_fail = false;
 
   template <typename T, typename ... Args>
-  static void call_if_signature(const Argument &o, const Args & ... args) {
-    if (std::holds_alternative<std::function<T>>(o.callback))
+  static bool call_if_signature(const Argument &o, const Args & ... args) {
+    if (std::holds_alternative<std::function<T>>(o.callback)) {
       std::get<std::function<T>>(o.callback)(args...);
+      return true;
+    }
+    return false;
   }
 
-  void help();
+  template <typename T, typename U>
+  static bool assign_if_reference(const Argument& o, const U& u) {
+    if (std::holds_alternative<std::reference_wrapper<T>>(o.callback)) {
+      std::get<std::reference_wrapper<T>>(o.callback).get() = u;
+      return true;
+    }
+    return false;
+  }
 
 public:
   Args(const std::string &name_, const std::string &description_, const std::string &version_)
@@ -53,6 +69,21 @@ public:
       exit(1);
     });
   }
+
+  void add(const std::string& s,
+           const std::string& l,
+           const std::string& d,
+           const std::reference_wrapper<bool>& value);
+
+  void add(const std::string& s,
+           const std::string& l,
+           const std::string& d,
+           const std::reference_wrapper<int>& value);
+
+  void add(const std::string& s,
+           const std::string& l,
+           const std::string& d,
+           const std::reference_wrapper<std::string>& value);
 
   void add(const std::string& s,
            const std::string& l,
@@ -70,6 +101,8 @@ public:
            const std::function<void(std::string)>& callback);
 
   void parse(int argc, char* argv[]);
+
+  void help();
 
 };
 
--- src/hort/args.cpp
+++ src/hort/args.cpp
@@ -1,6 +1,38 @@
 #include "hort/args.hpp"
 
+#include <fmt/format.h>
+
 namespace hort {
+
+void Args::add(const std::string& s,
+               const std::string& l,
+               const std::string& d,
+               const std::reference_wrapper<bool>& value) {
+  options.emplace_back(Argument{s, l, d, value});
+  if (max_width < l.size()) {
+    max_width = l.size();
+  }
+}
+
+void Args::add(const std::string& s,
+               const std::string& l,
+               const std::string& d,
+               const std::reference_wrapper<int>& value) {
+  options.emplace_back(Argument{s, l, d, value});
+  if (max_width < l.size()) {
+    max_width = l.size();
+  }
+}
+
+void Args::add(const std::string& s,
+               const std::string& l,
+               const std::string& d,
+               const std::reference_wrapper<std::string>& value) {
+  options.emplace_back(Argument{s, l, d, value});
+  if (max_width < l.size()) {
+    max_width = l.size();
+  }
+}
 
 void Args::add(const std::string& s,
                const std::string& l,
@@ -38,9 +70,12 @@ void Args::parse(int argc, char* argv[]) {
     std::string arg = argv[i];
     for (const auto& o : options) {
       if ((flag |= (arg == o.s || arg == o.l))) {
-        call_if_signature<void()>(o);
-        call_if_signature<void(int)>(o, std::stoi(argv[++i]));
-        call_if_signature<void(std::string)>(o, argv[++i]);
+        if (call_if_signature<void()>(o))                                  { i++;    break; }
+        if (call_if_signature<void(int)>(o, std::atoi(argv[i + 1])))       { i += 2; break; }
+        if (call_if_signature<void(std::string)>(o, argv[i + 1]))          { i += 2; break; }
+        if (assign_if_reference<bool>(o, std::atoi(argv[i + 1])))          { i += 2; break; }
+        if (assign_if_reference<int>(o, std::atoi(argv[i + 1])))           { i += 2; break; }
+        if (assign_if_reference<std::string>(o, std::string(argv[i + 1]))) { i += 2; break; }
       }
     }
     if (!flag) {
